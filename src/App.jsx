202ce167d94a73ad6454930729e708bad69a1f03
Ashley Le <ashleyle@usf.edu>
2025-04-13 18:20:54 -0400

    Setup root component with state and props structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+//Task 1: Setup Root Component (App.jsx)
+import React, {useState} from "react";
+import Gallery from "./components/Gallery";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [tours, setTours] = useState([]); //root component of app
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+const removeTour = (id) => {
+  setTours((prevTours) => prevTours.filter((tour) => tour.id !== id)); 
+}; //function to remove tour from its id
+
+return (
+  <main>
+    <h1>Tour Explorer</h1>
+    {/*passes state and handlers to Gallery component */}
+    <Gallery tours={tours} setTours={setTours} removeTour={removeTour} />
+  </main>
+);
 }
 
-export default App
+export default App;
